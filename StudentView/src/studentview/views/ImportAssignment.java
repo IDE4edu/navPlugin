7f0eb3e0b558ff861ce144e5421e92b9c4339aa8
Andrea Wu <andreaowu@berkeley.edu>
2012-11-16 16:36:24 -0800

    Still merging

---
+++ StudentView/src/studentview/views/ImportAssignment.java
@@ -0,0 +1,60 @@
+package studentview.views;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class ImportAssignment extends TitleAreaDialog {
+
+	public ImportAssignment(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	public void create() {
+		super.create();
+		// Set the title
+		setTitle("Choose Assignment to import");
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.marginBottom = 20;
+		layout.marginRight = 20;
+		layout.marginTop = 20;
+		layout.marginLeft = 20;
+		parent.setLayout(layout);
+		
+		return parent;
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+
+		// Create Cancel button
+		Button cancelButton = 
+				createButton(parent, CANCEL, "Cancel", false);
+		// Add a SelectionListener
+		cancelButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				setReturnCode(CANCEL);
+				close();
+			}
+		});
+	}
+
+
+	@Override
+	protected boolean isResizable() {
+		return true;
+	}
+
+} 
