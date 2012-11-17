2f219d2130b6cd6b05cc9543baf843b8e9ebfb87
Andrea Wu <andreaowu@berkeley.edu>
2012-11-16 16:35:16 -0800

    Merging in progress

--- StudentView/src/studentview/views/AssignmentChooser.java
+++ StudentView/src/studentview/views/AssignmentChooser.java
@@ -1,161 +1,139 @@
 package studentview.views;
 
-//import org.eclipse.jface.dialogs.IMessageProvider;
-import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.jface.resource.JFaceResources;
+/*
+ * - scroll container for list of isas -- make tall and narrow on left side, 
+ * because list could get long?
+ * hover over element of isa list should show introtext as well as location on filesystem
+ */
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+import org.eclipse.swt.widgets.Label;
+import java.util.ArrayList;
+
 import org.eclipse.swt.SWT;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
+
+import studentview.model.Assignment;
 
 public class AssignmentChooser extends TitleAreaDialog {
 
-  private Text firstNameText;
-  private Text lastNameText;
-  private String firstName;
-  private String lastName;
+	private ArrayList<Assignment> assignments;
+	private Assignment showAssignment;
+	private Shell parentShell;
+	
+	
+	public AssignmentChooser(Shell parentShell) {
+		super(parentShell);
+		assignments = new ArrayList<Assignment>();
+		this.parentShell = parentShell;
+		// Get all the assignments currently loaded in student's Eclipse
+		try {
+			ResourcesPlugin.getWorkspace().getRoot().accept(new IResourceVisitor() {			
+				@Override
+				public boolean visit(IResource resource) throws CoreException {
+					if (!(resource.getType() == IResource.FILE)) return true;
+					String extension = resource.getFileExtension();
+					if (extension != null) {
+						if (extension.equalsIgnoreCase("isa")) {
+							parseISA((IFile)resource);
+						}
+					}
+					return true;
+				}
+			});
+		} catch (CoreException e1) {
+			System.err.println("Core Exception!!!");
+			e1.printStackTrace();
+		}
+	}
 
-  public AssignmentChooser(Shell parentShell) {
-    super(parentShell);
-  }
+	@Override
+	public void create() {
+		super.create();
+		setTitle("Choose assignment to work on");
+	}
 
-  @Override
-  public void create() {
-    super.create();
-    // Set the title
-    setTitle("Choose assignment");
-    // Set the message
-    /*
-    setMessage("This is a TitleAreaDialog", 
-        IMessageProvider.INFORMATION);
-	*/
-  }
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.marginBottom = 20;
+		layout.marginRight = 20;
+		layout.marginTop = 20;
+		layout.marginLeft = 20;
+		parent.setLayout(layout);
+		
+		for (int i = 0; i < assignments.size(); i++) {
+			
+			Button radio = new Button(parent, SWT.RADIO);
+			radio.setText(assignments.get(i).getName());
+			radio.setToolTipText(assignments.get(i).getIntro());
+			radio.setEnabled(true);
+			
+			final int z = i;
+			radio.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					//show the Assignment
+					showAssignment = assignments.get(z);
+					setReturnCode(OK);
+					close();
+				}
+			});
+		}
+		return parent;
+	}
 
-  @Override
-  protected Control createDialogArea(Composite parent) {
-    GridLayout layout = new GridLayout();
-    layout.numColumns = 2;
-    // layout.horizontalAlignment = GridData.FILL;
-    parent.setLayout(layout);
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		Button importButton = new Button(parent, SWT.PUSH);
+		importButton.setText("Import assignment");
+		importButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				Shell shell = new Shell();
+				ImportAssignment dialog = new ImportAssignment(shell);
+				dialog.create();
+				dialog.open();
+			}
+		});
 
-    // The text fields will grow with the size of the dialog
-    GridData gridData = new GridData();
-    gridData.grabExcessHorizontalSpace = true;
-    gridData.horizontalAlignment = GridData.FILL;
+		// Create Cancel button
+		Button cancelButton = createButton(parent, CANCEL, "Cancel", false);
+		cancelButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				setReturnCode(CANCEL);
+				close();
+			}
+		});
+	}
 
-    Label label1 = new Label(parent, SWT.NONE);
-    label1.setText("First Name");
+	@Override
+	protected boolean isResizable() {
+		return true;
+	}
 
-    firstNameText = new Text(parent, SWT.BORDER);
-    firstNameText.setLayoutData(gridData);
-    
-    Label label2 = new Label(parent, SWT.NONE);
-    label2.setText("Last Name");
-    // You should not re-use GridData
-    gridData = new GridData();
-    gridData.grabExcessHorizontalSpace = true;
-    gridData.horizontalAlignment = GridData.FILL;
-    lastNameText = new Text(parent, SWT.BORDER);
-    lastNameText.setLayoutData(gridData);
-    return parent;
-  }
+	public void parseISA(IFile file) {
+		Assignment s = Assignment.parseISA(file);
+		if (s == null) System.err.println("Failed to parse file: " + file.getName());
+		s.getIntro();
+		assignments.add(s);
+	}
 
-  @Override
-  protected void createButtonsForButtonBar(Composite parent) {
-    GridData gridData = new GridData();
-    gridData.verticalAlignment = GridData.FILL;
-    gridData.horizontalSpan = 3;
-    gridData.grabExcessHorizontalSpace = true;
-    gridData.grabExcessVerticalSpace = true;
-    gridData.horizontalAlignment = SWT.CENTER;
-
-    parent.setLayoutData(gridData);
-    // Create Add button
-    // Own method as we need to overview the SelectionAdapter
-    createOkButton(parent, OK, "Add", true);
-    // Add a SelectionListener
-
-    // Create Cancel button
-    Button cancelButton = 
-        createButton(parent, CANCEL, "Cancel", false);
-    // Add a SelectionListener
-    cancelButton.addSelectionListener(new SelectionAdapter() {
-      public void widgetSelected(SelectionEvent e) {
-        setReturnCode(CANCEL);
-        close();
-      }
-    });
-  }
-
-  protected Button createOkButton(Composite parent, int id, 
-      String label,
-      boolean defaultButton) {
-    // increment the number of columns in the button bar
-    ((GridLayout) parent.getLayout()).numColumns++;
-    Button button = new Button(parent, SWT.PUSH);
-    button.setText(label);
-    button.setFont(JFaceResources.getDialogFont());
-    button.setData(new Integer(id));
-    button.addSelectionListener(new SelectionAdapter() {
-      public void widgetSelected(SelectionEvent event) {
-        if (isValidInput()) {
-          okPressed();
-        }
-      }
-    });
-    if (defaultButton) {
-      Shell shell = parent.getShell();
-      if (shell != null) {
-        shell.setDefaultButton(button);
-      }
-    }
-    setButtonLayoutData(button);
-    return button;
-  }
-
-  private boolean isValidInput() {
-    boolean valid = true;
-    if (firstNameText.getText().length() == 0) {
-      setErrorMessage("Please maintain the first name");
-      valid = false;
-    }
-    if (lastNameText.getText().length() == 0) {
-      setErrorMessage("Please maintain the last name");
-      valid = false;
-    }
-    return valid;
-  }
-  
-  @Override
-  protected boolean isResizable() {
-    return true;
-  }
-
-  // Coyy textFields because the UI gets disposed
-  // and the Text Fields are not accessible any more.
-  private void saveInput() {
-    firstName = firstNameText.getText();
-    lastName = lastNameText.getText();
-  }
-
-  @Override
-  protected void okPressed() {
-    saveInput();
-    super.okPressed();
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
+	public Assignment getSegment() {
+		return showAssignment;
+	}
 } 
