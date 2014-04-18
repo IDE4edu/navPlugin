eaa12a98f4ee593eda769ca3b7b8e04ba5ed9f42
Nathaniel Titterton <nate@berkeley.edu>
2014-04-18 14:54:43 -0700

    isa parse stuff moved to eduridebase util methods

--- StudentView/src/navigatorView/views/ActivityChooser.java
+++ StudentView/src/navigatorView/views/ActivityChooser.java
@@ -39,32 +39,43 @@ public class ActivityChooser extends TitleAreaDialog {
 		findActivitiesInWorkspace();
 	}
 
+	// TODO use EduRideBase.ISAUtil stuff!!  hashmap, etc
 	private void findActivitiesInWorkspace() {
 		activities.clear();
-		try {
-			ResourcesPlugin.getWorkspace().getRoot()
-					.accept(new IResourceVisitor() {
-						@Override
-						public boolean visit(IResource resource)
-								throws CoreException {
-							if (!(resource.getType() == IResource.FILE))
-								return true;
-							String extension = resource.getFileExtension();
-							if (extension != null) {
-								if (extension.equalsIgnoreCase("isa")) {
-									parseISA((IFile) resource);
-								}
-							}
-							return true;
-						}
-					});
-		} catch (CoreException e) {
-			Console.err(e);
-		}
+//		try {
+//			ResourcesPlugin.getWorkspace().getRoot()
+//					.accept(new IResourceVisitor() {
+//						@Override
+//						public boolean visit(IResource resource)
+//								throws CoreException {
+//							if (!(resource.getType() == IResource.FILE))
+//								return true;
+//							String extension = resource.getFileExtension();
+//							if (extension != null) {
+//								if (extension.equalsIgnoreCase("isa")) {
+//									parseISA((IFile) resource);
+//								}
+//							}
+//							return true;
+//						}
+//					});
+//		} catch (CoreException e) {
+//			Console.err(e);
+//		}
+		Collections.copy(activities, Activity.getActivities());
 		// sort activities wrt sortOrder
 		Collections.sort(activities);
 	}
 
+	// TODO this goes away, uses stuff in ISAUtil, activityStore, etc...
+//	public void parseISA(IFile file) {
+//		Activity s = ISAUtil.parseISA(file);
+//		if (s != null) {
+//			activities.add(s);
+//		}
+//	}
+	
+	
 	@Override
 	public void create() {
 		super.create();
@@ -178,12 +189,6 @@ public class ActivityChooser extends TitleAreaDialog {
 		return true;
 	}
 
-	public void parseISA(IFile file) {
-		Activity s = ISAUtil.parseISA(file);
-		if (s != null) {
-			activities.add(s);
-		}
-	}
 
 	public Activity getActivity() {
 		return showActivity;
